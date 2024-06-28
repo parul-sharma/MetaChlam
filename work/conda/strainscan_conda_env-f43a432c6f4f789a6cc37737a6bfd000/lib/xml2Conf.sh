0e4c61372515086035751776777f4fde7192c0b0
parul-sharma <parul15123@iiitd.ac.in>
2024-06-28 18:19:58 +0000

    clean up the repo

--- work/conda/strainscan_conda_env-f43a432c6f4f789a6cc37737a6bfd000/lib/xml2Conf.sh
+++
@@ -1,8 +0,0 @@
-#
-# Configuration file for using the XML library in GNOME applications
-#
-XML2_LIBDIR="-L/workspaces/MetaChlam/work/conda/strainscan_conda_env-f43a432c6f4f789a6cc37737a6bfd000/lib"
-XML2_LIBS="-lxml2 -L/workspaces/MetaChlam/work/conda/strainscan_conda_env-f43a432c6f4f789a6cc37737a6bfd000/lib -lz -L/workspaces/MetaChlam/work/conda/strainscan_conda_env-f43a432c6f4f789a6cc37737a6bfd000/lib -llzma -lpthread -L/workspaces/MetaChlam/work/conda/strainscan_conda_env-f43a432c6f4f789a6cc37737a6bfd000/lib -liconv -L/workspaces/MetaChlam/work/conda/strainscan_conda_env-f43a432c6f4f789a6cc37737a6bfd000/lib -licui18n -licuuc -licudata -lm "
-XML2_INCLUDEDIR="-I/workspaces/MetaChlam/work/conda/strainscan_conda_env-f43a432c6f4f789a6cc37737a6bfd000/include/libxml2 -I/workspaces/MetaChlam/work/conda/strainscan_conda_env-f43a432c6f4f789a6cc37737a6bfd000/include"
-MODULE_VERSION="xml2-2.9.10"
-
